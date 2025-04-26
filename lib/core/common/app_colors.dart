1bb5c191c41ba1f857398853700a9a7facecf095
Gaurav Varadkar <gaurav.varadkar@blink.in>
2025-04-26 11:18:23 +0530

    app colors styling added

--- lib/core/common/app_colors.dart
+++ lib/core/common/app_colors.dart
@@ -2,4 +2,6 @@ import 'dart:ui';
 
 class AppColors {
   static const primaryColor = Color(0xFF3F51B5);
+
+  static const textBlackColor = Color(0xFF000000);
 }
---
+++ lib/core/common/app_fontStyles.dart
@@ -0,0 +1,381 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:swapgo/core/common/app_colors.dart';
+
+class AppTextStyle {
+  static TextStyle font9Regular({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 9,
+      fontWeight: FontWeight.w400,
+      color: color,
+    );
+  }
+
+  static TextStyle font9Medium({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 9,
+      fontWeight: FontWeight.w500,
+      color: color,
+    );
+  }
+
+  static TextStyle font9SemiBold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 9,
+      fontWeight: FontWeight.w600,
+      color: color,
+    );
+  }
+
+  static TextStyle font9Bold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 9,
+      fontWeight: FontWeight.w700,
+      color: color,
+    );
+  }
+
+  static TextStyle font10Regular({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 10,
+      fontWeight: FontWeight.w400,
+      color: color,
+    );
+  }
+
+  static TextStyle font10Medium({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 10,
+      fontWeight: FontWeight.w500,
+      color: color,
+    );
+  }
+
+  static TextStyle font10SemiBold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 10,
+      fontWeight: FontWeight.w600,
+      color: color,
+    );
+  }
+
+  static TextStyle font10Bold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 10,
+      fontWeight: FontWeight.w700,
+      color: color,
+    );
+  }
+
+  static TextStyle font11Regular({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 11,
+      fontWeight: FontWeight.w400,
+      color: color,
+    );
+  }
+
+  static TextStyle font11Medium({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 11,
+      fontWeight: FontWeight.w500,
+      color: color,
+    );
+  }
+
+  static TextStyle font11SemiBold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 11,
+      fontWeight: FontWeight.w600,
+      color: color,
+    );
+  }
+
+  static TextStyle font11Bold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 11,
+      fontWeight: FontWeight.w700,
+      color: color,
+    );
+  }
+
+  static TextStyle font12Regular({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      color: color,
+    );
+  }
+
+  static TextStyle font12Medium({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 12,
+      fontWeight: FontWeight.w500,
+      color: color,
+    );
+  }
+
+  static TextStyle font12SemiBold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 12,
+      fontWeight: FontWeight.w600,
+      color: color,
+    );
+  }
+
+  static TextStyle font12Bold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 12,
+      fontWeight: FontWeight.w700,
+      color: color,
+    );
+  }
+
+  static TextStyle font13Regular({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 13,
+      fontWeight: FontWeight.w400,
+      color: color,
+    );
+  }
+
+  static TextStyle font13Medium({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 13,
+      fontWeight: FontWeight.w500,
+      color: color,
+    );
+  }
+
+  static TextStyle font13SemiBold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 13,
+      fontWeight: FontWeight.w600,
+      color: color,
+    );
+  }
+
+  static TextStyle font13Bold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 13,
+      fontWeight: FontWeight.w700,
+      color: color,
+    );
+  }
+
+  static TextStyle font14Regular({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      color: color,
+    );
+  }
+
+  static TextStyle font14Medium({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      color: color,
+    );
+  }
+
+  static TextStyle font14SemiBold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 14,
+      fontWeight: FontWeight.w600,
+      color: color,
+    );
+  }
+
+  static TextStyle font14Bold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+      color: color,
+    );
+  }
+
+  static TextStyle font15Regular({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 15,
+      fontWeight: FontWeight.w400,
+      color: color,
+    );
+  }
+
+  static TextStyle font15Medium({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 15,
+      fontWeight: FontWeight.w500,
+      color: color,
+    );
+  }
+
+  static TextStyle font15SemiBold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 15,
+      fontWeight: FontWeight.w600,
+      color: color,
+    );
+  }
+
+  static TextStyle font15Bold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 15,
+      fontWeight: FontWeight.w700,
+      color: color,
+    );
+  }
+
+  static TextStyle font16Regular({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+      color: color,
+    );
+  }
+
+  static TextStyle font16Medium({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 16,
+      fontWeight: FontWeight.w500,
+      color: color,
+    );
+  }
+
+  static TextStyle font16SemiBold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 16,
+      fontWeight: FontWeight.w600,
+      color: color,
+    );
+  }
+
+  static TextStyle font16Bold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 16,
+      fontWeight: FontWeight.w700,
+      color: color,
+    );
+  }
+
+  static TextStyle font17Regular({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 17,
+      fontWeight: FontWeight.w400,
+      color: color,
+    );
+  }
+
+  static TextStyle font17Medium({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 17,
+      fontWeight: FontWeight.w500,
+      color: color,
+    );
+  }
+
+  static TextStyle font17SemiBold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 17,
+      fontWeight: FontWeight.w600,
+      color: color,
+    );
+  }
+
+  static TextStyle font17Bold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 17,
+      fontWeight: FontWeight.w700,
+      color: color,
+    );
+  }
+
+  static TextStyle font18Regular({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 18,
+      fontWeight: FontWeight.w400,
+      color: color,
+    );
+  }
+
+  static TextStyle font18Medium({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 18,
+      fontWeight: FontWeight.w500,
+      color: color,
+    );
+  }
+
+  static TextStyle font18SemiBold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 18,
+      fontWeight: FontWeight.w600,
+      color: color,
+    );
+  }
+
+  static TextStyle font18Bold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 18,
+      fontWeight: FontWeight.w700,
+      color: color,
+    );
+  }
+
+  static TextStyle font18ExtraBold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 18,
+      fontWeight: FontWeight.w800,
+      color: color,
+    );
+  }
+
+  static TextStyle font20Regular({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 20,
+      fontWeight: FontWeight.w400,
+      color: color,
+    );
+  }
+
+  static TextStyle font20Medium({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 20,
+      fontWeight: FontWeight.w500,
+      color: color,
+    );
+  }
+
+  static TextStyle font20SemiBold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 20,
+      fontWeight: FontWeight.w600,
+      color: color,
+    );
+  }
+
+  static TextStyle font20Bold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 20,
+      fontWeight: FontWeight.w700,
+      color: color,
+    );
+  }
+
+  static TextStyle font24Bold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 24,
+      fontWeight: FontWeight.w700,
+      color: color,
+    );
+  }
+
+  static TextStyle font26Bold({Color color = AppColors.textBlackColor}) {
+    return GoogleFonts.raleway(
+      fontSize: 26,
+      fontWeight: FontWeight.w700,
+      color: color,
+    );
+  }
+}
